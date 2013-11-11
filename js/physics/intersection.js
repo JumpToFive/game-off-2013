3da22659475252d60a4b30d3fbc37ad6b7fab847
Raymond Zhou <somecrazyidiot@gmail.com>
2013-11-11 00:40:14 -0500

    Scaffold out some line/segment and circle intersection functions.

--- js/physics/intersection.js
+++ js/physics/intersection.js
@@ -15,6 +15,10 @@ define([
   }
 
   function lineParameter( x0, y0, x1, y1, parameter ) {
+    if ( parameter === null ) {
+      return null;
+    }
+
     return {
       x: Utils.lerp( x0, x1, parameter ),
       y: Utils.lerp( y0, y1, parameter )
@@ -32,10 +36,6 @@ define([
 
   function lineIntersection( x0, y0, x1, y1, x2, y2, x3, y3 ) {
     var t = lineIntersectionParameter( x0, y0, x1, y1, x2, y2, x3, y3 );
-    if ( t === null ) {
-      return null;
-    }
-
     return lineParameter( x0, y0, x1, y1, t );
   }
 
@@ -57,14 +57,39 @@ define([
 
   function segmentIntersection( x0, y0, x1, y1, x2, y2, x3, y3 ) {
     var t = segmentIntersectionParameter( x0, y0, x1, y1, x2, y2, x3, y3 );
+    return lineParameter( x0, y0, x1, y1, t );
+  }
+
+  function lineCircleIntersectionParameter( x0, y0, x1, y1, cx, cy, r ) {
+    // Transform line to circle space.
+    x0 -= cx;
+    y0 -= cy;
+    x1 -= cx;
+    y1 -= cy;
+  }
+
+  // Should return an array?
+  function lineCircleIntersection( x0, y0, x1, y1, cx, cy, r ) {
+    var t = lineCircleIntersectionParameter( x0, y0, x1, y1, cx, cy, r );
+    return lineParameter( x0, y0, x1, y1, t );
+  }
+
+  function segmentCircleIntersectionParameter( x0, y0, x1, y1, cx, cy, r ) {
+    var t = lineCircleIntersectionParameter( x0, y0, x1, y1, cx, cy, r );
     if ( t === null ) {
       return null;
     }
 
-    return lineParameter( x0, y0, x1, y1, t );
+    if ( 0 > t || t > 1 ) {
+      return null;
+    }
+
+    return t;
   }
 
-  function lineCircleIntersectionParameter( x0, y0, x1, y1, cx, cy, radius ) {
+  function segmentCircleIntersection( x0, y0, x1, y1, cx, cy, r ) {
+    var t = segmentCircleIntersectionParameter( x0, y0, x1, y1, cx, cy, r );
+    return lineParameter( x0, y0, x1, y1, t );
   }
 
   return {
@@ -76,6 +101,12 @@ define([
     lineIntersection: lineIntersection,
 
     segmentIntersectionParameter: segmentIntersectionParameter,
-    segmentIntersection: segmentIntersection
+    segmentIntersection: segmentIntersection,
+
+    lineCircleIntersectionParameter: lineCircleIntersectionParameter,
+    lineCircleIntersection: lineCircleIntersection,
+
+    segmentCircleIntersectionParameter: segmentCircleIntersectionParameter,
+    segmentCircleIntersection: segmentCircleIntersection
   };
 });
