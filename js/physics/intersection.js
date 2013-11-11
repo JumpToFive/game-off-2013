49680690e4969d7319d3013033369d2e3f1cc6eb
Raymond Zhou <somecrazyidiot@gmail.com>
2013-11-10 23:29:07 -0500

    Add some more Intersection utility functions.

--- js/physics/intersection.js
+++ js/physics/intersection.js
@@ -1,5 +1,7 @@
 /*globals define*/
-define(function() {
+define([
+  'utils'
+], function( Utils ) {
   'use strict';
 
   /**
@@ -10,6 +12,41 @@ define(function() {
            a.xmax >= b.xmin &&
            a.ymin <= b.ymax &&
            a.ymax >= b.ymin;
+  }
+
+  function lineIntersectionParameter( x0, y0, x1, y1, x2, y2, x3, y3 ) {
+
+  }
+
+  function lineIntersection( x0, y0, x1, y1, x2, y2, x3, y3 ) {
+    var t = lineIntersectionParameter.apply( null, arguments );
+    if ( t === null ) {
+      return null;
+    }
+
+    return {
+      x: Utils.lerp( x0, x1, t ),
+      y: Utils.lerp( y0, y1, t )
+    };
+  }
+
+  function segmentIntersectionParameter( x0, y0, x1, y1, x2, y2, x3, y3 ) {
+
+  }
+
+  function segmentIntersection( x0, y0, x1, y1, x2, y2, x3, y3 ) {
+    var t = segmentIntersectionParameter.apply( null, argmuents );
+    if ( t === null ) {
+      return null;
+    }
+
+    return {
+      x: Utils.lerp( x0, y1, t ),
+      y: Utils.lerp( y0, y1, t )
+    };
+  }
+
+  function lineCircleIntersectionParameter( x0, y0, x1, y1, x, y, radius ) {
   }
 
   return {
--- js/utils.js
+++ js/utils.js
@@ -2,11 +2,14 @@
 define(function() {
   'use strict';
 
+  var EPSILON = 1e-5;
+
   function lerp( a, b, t ) {
     return a + t * ( b - a );
   }
 
   return {
+    EPSILON: EPSILON,
     lerp: lerp
   };
 });
