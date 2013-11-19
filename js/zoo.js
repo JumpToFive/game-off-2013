16116a5d837245545555cbdeb79bd48d624361e3
Raymond Zhou <somecrazyidiot@gmail.com>
2013-11-19 17:59:19 -0500

    zoo: Not visually appealing atom-bot thing.

--- js/zoo.js
+++ js/zoo.js
@@ -1,6 +1,8 @@
 (function( window, document, undefined ) {
   'use strict';
 
+  var PI2 = 2 * Math.PI;
+
   var running = false;
 
   var el = document.querySelector( '.container' );
@@ -11,8 +13,13 @@
     var canvas = document.createElement( 'canvas' ),
         ctx    = canvas.getContext( '2d' );
 
-    canvas.width = 64;
-    canvas.height = 64;
+    var size = 128;
+
+    var width  = size,
+        height = size;
+
+    canvas.width  = width;
+    canvas.height = height;
     canvas.style.backgroundColor = 'black';
 
     el.appendChild( canvas );
@@ -20,19 +27,37 @@
     function draw() {
       ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height );
 
+      // Draw main body.
       ctx.beginPath();
-      ctx.arc( 32, 32, 20, 0, 2 * Math.PI );
+      ctx.arc( 0.5 * width, 0.5 * height, 0.2 * width, 0, PI2 );
+      ctx.fillStyle = '#ddf';
+      ctx.fill();
 
-      ctx.shadowColor = 'white';
-      ctx.shadowBlur = 8 + Math.random() * 4;
+      // Draw eye.
+      ctx.beginPath();
+      ctx.arc( 0.65 * width, 0.5 * height, 0.08 * width, 0, PI2 );
+      ctx.fillStyle = '#f43';
+      ctx.fill();
 
-      ctx.lineWidth = 8;
-      ctx.strokeStyle = 'white';
+      // Draw eye gleam.
+      ctx.beginPath();
+      ctx.arc( 0.65 * width, 0.5 * height, 0.05 * width, -0.25 * Math.PI, 0.25 * Math.PI );
+      ctx.lineWidth = 0.02 * width;
+      ctx.strokeStyle = '#fff';
+      ctx.stroke();
+
+      // Draw ring.
+      ctx.beginPath();
+      ctx.arc( 0.5 * width, 0.5 * height, 0.35 * width, 0, PI2 );
+
+      ctx.shadowColor = '#fff';
+      ctx.shadowBlur = 0.05 * width + Math.random() * 0.03 * width;
+
+      ctx.lineWidth = 0.07 * width;
+      ctx.strokeStyle = '#fff';
       ctx.stroke();
 
       ctx.shadowBlur = 0;
-      ctx.fillStyle = '#89c';
-      ctx.fill();
     }
 
     return function() {
