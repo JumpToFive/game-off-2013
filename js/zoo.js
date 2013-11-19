cabf0683f80c98935e9a7b2396f59fe000f424a1
Raymond Zhou <somecrazyidiot@gmail.com>
2013-11-19 17:41:47 -0500

    First zoo test: glowing white ring.

--- js/zoo.js
+++ js/zoo.js
@@ -1,18 +1,75 @@
 (function( window, document, undefined ) {
   'use strict';
 
+  var running = false;
+
   var el = document.querySelector( '.container' );
 
-  (function() {
+  var tickFns = [];
+
+  tickFns.push((function() {
     var canvas = document.createElement( 'canvas' ),
         ctx    = canvas.getContext( '2d' );
 
     canvas.width = 64;
     canvas.height = 64;
+    canvas.style.backgroundColor = 'black';
 
     el.appendChild( canvas );
 
-    ctx.fillStyle = 'black';
-    ctx.fillRect( 0, 0, ctx.canvas.width, ctx.canvas.height );
-  }) ();
+    function draw() {
+      ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height );
+
+      ctx.beginPath();
+      ctx.arc( 32, 32, 20, 0, 2 * Math.PI );
+
+      ctx.shadowColor = 'white';
+      ctx.shadowBlur = 8 + Math.random() * 4;
+
+      ctx.lineWidth = 8;
+      ctx.strokeStyle = 'white';
+      ctx.stroke();
+
+      ctx.shadowBlur = 0;
+      ctx.fillStyle = '#89c';
+      ctx.fill();
+    }
+
+    return function() {
+      draw();
+    };
+  }) ());
+
+  function tick() {
+    tickFns.forEach(function( tickFn ) {
+      tickFn();
+    });
+
+    if ( !running ) {
+      return;
+    }
+
+    window.requestAnimationFrame( tick );
+  }
+
+  tick();
+
+  document.addEventListener( 'keydown', function( event ) {
+    // ESC.
+    if ( event.which === 27 ) {
+      running = false;
+    }
+
+    // Space.
+    if ( event.which === 32 ) {
+      if ( !running ) {
+        running = true;
+        tick();
+      }
+    }
+  });
+
+  window.addEventListener( 'blur', function() {
+    running = false;
+  });
 }) ( window, document );
