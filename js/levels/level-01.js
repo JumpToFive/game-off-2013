5be14c75715e945f069c2795021115d72df54a16
Raymond Zhou <somecrazyidiot@gmail.com>
2013-12-01 01:21:57 -0500

    Add first version of Level 01.

--- js/levels/level-01.js
+++ js/levels/level-01.js
@@ -6,11 +6,19 @@ define(function( require ) {
   var Level = require( 'level' );
   var Utils = require( 'utils' );
 
+  var Door = require( 'entities/door' );
   var Laser = require( 'entities/laser' );
+  var Emitter = require( 'entities/emitter' );
   var TractorBeam = require( 'entities/tractor-beam' );
+  var Trigger = require( 'entities/trigger' );
+
+  var Polygon = require( 'geometry/polygon' );
+  var Segment = require( 'geometry/segment' );
 
   var Trail = require( 'effects/trail' );
+  var TriggerWire = require( 'effects/trigger-wire' );
 
+  var Colors = require( 'config/colors' );
   var Material = require( 'config/material' );
 
   var DEG_TO_RAD = Utils.DEG_TO_RAD;
@@ -19,10 +27,9 @@ define(function( require ) {
 
   return function( game ) {
     game.clear();
-    game.debug = true;
 
-    game.player.x = -65;
-    game.player.y = 15;
+    game.player.x = -63;
+    game.player.y = 13;
 
     var trail = new Trail();
     trail.fill = new Color( 255, 255, 255, 0.2 );
@@ -54,8 +61,120 @@ define(function( require ) {
     tractorBeam.force = 3000;
     game.add( tractorBeam );
 
+    // Initial laser.
     var laser0 = new Laser( -17, 46, Material.ANTIMATTER );
     laser0.angle = 60 * DEG_TO_RAD;
     game.add( laser0 );
+
+    // Laser web.
+    var laser1 = new Laser( 16, 48, Material.MATTER );
+    laser1.angle = 90 * DEG_TO_RAD;
+    game.add( laser1 );
+
+    var laser2 = new Laser( 28, 42, Material.ANTIMATTER );
+    laser2.angle = 130 * DEG_TO_RAD;
+    game.add( laser2 );
+
+    var laser3 = new Laser( 13, 17, Material.ANTIMATTER );
+    laser3.angle = -40 * DEG_TO_RAD;
+    game.add( laser3 );
+
+    var laser4 = new Laser( 18, 13, Material.MATTER );
+    laser4.angle = -20 * DEG_TO_RAD;
+    game.add( laser4 );
+
+    // Trash.
+    var trashAM = new Polygon();
+    trashAM.vertices = [ 0.75, 0.75, -0.75, 0.75, -0.75, -0.75, 0.75, -0.75 ];
+    trashAM.fill.set( Colors.Solid.ANTIMATTER );
+    trashAM.stroke.set( Colors.White );
+    trashAM.lineWidth = 0.2;
+
+    // Emitters.
+    var em0 = new Emitter( 35, -15 );
+    em0.spawnArea = new Segment( 0, -2, 0, 2 );
+    em0.rate = 0.4;
+    em0.lifeTime = 2;
+    em0.speed = 100;
+    em0.angle = -110 * DEG_TO_RAD;
+
+    em0.particle = trashAM;
+    em0.properties = {
+      shape: 'polygon',
+      type: 'vector',
+      data: trashAM.vertices.slice(),
+      fixture: {
+        density: 1.75,
+        friction: 0.5,
+        restitution: 0.2,
+        filter: {
+          categoryBits: Material.ANTIMATTER
+        }
+      },
+      body: {
+        angularVelocity: 3 * Math.PI,
+        linearDamping: 0.2,
+        type: 'dynamic'
+      }
+    };
+
+    em0.start( 500 );
+    game.add( em0 );
+
+    var em1 = new Emitter( -2, -42 );
+    em1.spawnArea = new Segment( 0, -2, 0, 2 );
+    em1.rate = 0.2;
+    em1.lifeTime = 3;
+    em1.speed = 20;
+    em1.angle = -80 * DEG_TO_RAD;
+
+    em1.particle = trashAM;
+    em1.properties = {
+      shape: 'polygon',
+      type: 'vector',
+      data: trashAM.vertices.slice(),
+      fixture: {
+        density: 1,
+        friction: 0.5,
+        restitution: 0.2,
+        filter: {
+          categoryBits: Material.ANTIMATTER
+        }
+      },
+      body: {
+        angularVelocity: 3 * Math.PI,
+        linearDamping: 0.2,
+        type: 'dynamic'
+      }
+    };
+
+    em1.start( 1000 );
+    game.add( em1 );
+
+    // Tractor beam leading to trigger.
+    var tB1 = new TractorBeam( 35, -33, 20, 5 );
+    tB1.angle = -180 * DEG_TO_RAD;
+    tB1.force = 3000;
+    game.add( tB1 );
+
+    // Trigger.
+    var trig0 = new Trigger( -15, -30, 3, Material.ANTIMATTER );
+    game.add( trig0 );
+
+    // Door.
+    var door = new Door( -3, -15, 3 );
+    door.triggers.push( trig0 );
+    game.add( door );
+
+    // Trigger Wire.
+    var triggerWire = new TriggerWire( trig0, door, {
+      vertices: [
+        0, 0.5,
+        1, 0.5
+      ],
+      sourceDirection: TriggerWire.Direction.BOTTOM,
+      targetDirection: TriggerWire.Direction.Top
+    });
+    game.add( triggerWire );
   };
 });
