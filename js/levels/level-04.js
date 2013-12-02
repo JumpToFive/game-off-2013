f102545d825b04080a595c5e1c0b6ae4350f8040
Raymond Zhou <somecrazyidiot@gmail.com>
2013-12-01 22:09:55 -0500

    Preliminary work on Level 04.

--- js/levels/level-04.js
+++ js/levels/level-04.js
@@ -6,7 +6,9 @@ define(function( require ) {
   var LevelUtils = require( 'levels/utils-level' );
 
   var Door = require( 'entities/door' );
+  var Laser = require( 'entities/laser' );
   var Emitter = require( 'entities/emitter' );
+  var TractorBeam = require( 'entities/tractor-beam' );
   var Trigger = require( 'entities/trigger' );
   var TriggerWire = require( 'effects/trigger-wire' );
 
@@ -20,58 +22,54 @@ define(function( require ) {
   return function( game ) {
     game.clear();
 
-    game.player.x = 0;
-    game.player.y = 0;
-    game.camera.fill.alpha = 1;
+    game.player.x = -54;
+    game.player.y = -35;
 
-    game.background.hueSpread = 20;
+    game.background.hueSpread = 10;
+    game.background.lightnessSpread = 15;
 
     LevelUtils.playerMaterialOn( game );
     LevelUtils.addTrail( game );
-    LevelUtils.addBackground( game, 75, 44, 12, 1 );
+    LevelUtils.addBackground( game, 75, 34, 12, 1 );
     LevelUtils.loadData( game, level04Data );
 
-    game.camera.setHeight( 48, {
+    game.camera.setHeight( 33, {
       maintainAspectRatio: true
     });
+
+    // Laser.
+    var laser0 = new Laser( -57, 7, Material.ANTIMATTER );
+    laser0.angle = -60 * DEG_TO_RAD;
+    game.add( laser0 );
+
+    // Tractor beam.
+    var tractorBeam = new TractorBeam( -55, 22, 20, 5 );
+    tractorBeam.angle = -80 * DEG_TO_RAD;
+    tractorBeam.force = 2000;
+    game.add( tractorBeam );
 
     // Trash.
     var trashM = LevelUtils.normalTrash( Material.MATTER );
     var trashAM = LevelUtils.normalTrash( Material.ANTIMATTER );
 
     // Emitters.
-    var em0 = new Emitter( 20, -20 );
+    var em0 = new Emitter( -37, -9 );
     em0.spawnArea = LevelUtils.normalSpawnArea(4);
-    em0.rate = 0.2;
-    em0.lifeTime = 2;
+    em0.rate = 0.8;
+    em0.lifeTime = 9;
     em0.speed = 200;
-    em0.angle = -120 * DEG_TO_RAD;
+    em0.angle = -90 * DEG_TO_RAD;
 
-    em0.particle = trashM;
-    em0.properties = LevelUtils.normalTrashProperties( trashM, Material.MATTER );
+    em0.particle = trashAM;
+    em0.properties = LevelUtils.normalTrashProperties( trashAM, Material.ANTIMATTER );
+    em0.properties.fixture.restitution = 0.5;
 
     em0.start();
     game.add( em0 );
 
-    var em1 = new Emitter( -20, -20 );
-    em1.spawnArea = LevelUtils.normalSpawnArea(4);
-    em1.rate = 0.2;
-    em1.lifeTime = 2;
-    em1.speed = 200;
-    em1.angle = -60 * DEG_TO_RAD;
-
-    em1.particle = trashAM;
-    em1.properties = LevelUtils.normalTrashProperties( trashAM, Material.ANTIMATTER );
-
-    em1.start();
-    game.add( em1 );
-
     // Triggers.
-    var trig0 = new Trigger( 20, 20, 3, Material.ANTIMATTER );
+    var trig0 = new Trigger( -20, 35, 3, Material.ANTIMATTER );
     game.add( trig0 );
-
-    var trig1 = new Trigger( -20, 20, 3 , Material.MATTER );
-    game.add( trig1 );
 
     // Door.
     var door = new Door( 0, 40, 3, {
@@ -80,7 +78,6 @@ define(function( require ) {
       }
     });
     door.triggers.push( trig0 );
-    door.triggers.push( trig1 );
     game.add( door );
 
     // Trigger wires.
@@ -94,12 +91,5 @@ define(function( require ) {
       targetDirection: TriggerWire.Direction.RIGHT
     });
     game.add( wire0 );
-
-    var wire1 = new TriggerWire( trig1, door, {
-      vertices: wire0.vertices.slice(),
-      sourceDirection: TriggerWire.Direction.BOTTOM,
-      targetDirection: TriggerWire.Direction.LEFT
-    });
-    game.add( wire1 );
   };
 });
